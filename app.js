a7851df063431c536962b04aea1ef8c87b39ceb4
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-09-20 16:55:07 -0400

    Complete Part 3 - Registration

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import expressSession from "express-session";
 import mongoose from "mongoose";
+import passport from "passport";
+import bodyParser from "body-parser";
+import LocalStrategy from "passport-local";
+import passportLocalMongoose from "passport-local-mongoose";
 
-const app = express();
-app.set("view engine", 'ejs');
+import User from "./models/user.js";
 
 //fix mongoose deprecation warnings:
 mongoose.set('useNewUrlParser', true);
@@ -10,9 +14,24 @@ mongoose.set('useUnifiedTopology', true);
 //connect to database:
 mongoose.connect("mongodb://localhost:27017/auth_demo");
 
+const app = express();
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(expressSession({
+	secret: "Rusty is the best and cutest dog in the world",
+	resave: false,
+	saveUninitialized: false
+}));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//======
 //ROUTES
+//======
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -21,6 +40,29 @@ app.get("/", (req, res) => {
 app.get("/secret", (req, res) => {
 	res.render("secret");
 });
+
+
+//Auth Routes
+
+//show sign up form
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+//handle user signup
+app.post("/register", (req, res) => {
+	User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+		if(err) {
+			console.error(err);
+			res.render('register');
+		} else {
+			passport.authenticate("local")(req, res, () => {
+				res.redirect("/secret");
+			});
+		}
+	});
+});
+
 
 
 
