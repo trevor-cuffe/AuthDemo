16540ec7b0be7d92feb29e4736fb4f43108f003f
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-09-21 11:08:53 -0400

    Create authentication from scratch

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import expressSession from "express-session";
+import sessions from "client-sessions";
 import mongoose from "mongoose";
-import passport from "passport";
 import bodyParser from "body-parser";
-import LocalStrategy from "passport-local";
-import passportLocalMongoose from "passport-local-mongoose";
+import bcrypt from "bcryptjs";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 
 import User from "./models/user.js";
 
@@ -18,17 +18,54 @@ const app = express();
 app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(expressSession({
-	secret: "This is so random...",
-	resave: false,
-	saveUninitialized: false
+app.use(sessions({
+	cookieName: "session",
+	secret: "asdlkhv9x02k4h1493098dfckzu3l5nzuuuewl2cbi2",
+	duration: 30*60*1000	//30 mins
 }));
+app.use(cookieParser());
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-app.use(passport.initialize());
-app.use(passport.session());
+
+//Set standard functions for app requests:
+//Set user variable in sessions
+app.use((req, res, next) => {
+	if (!(req.session && req.session.userId)) {
+		return next();
+	}
+
+	User.findById(req.session.userId, (err, user) => {
+		if (err) {
+			return next(err);
+		}
+
+		if (!user) {
+			return next();
+		}
+
+		user.password = undefined;
+
+		req.user = user;
+		res.locals.user = user;
+
+		next();
+	});
+});
+
+function loginRequired(req, res, next) {
+	if (!req.user) {
+		return res.redirect("/login");
+	}
+
+	next();
+}
+
+let csrfProtection = csrf({ cookie: true });
+
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //======
 //ROUTES
@@ -38,60 +75,70 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 
-app.get("/secret", isLoggedIn, (req, res) => {
-	res.render("secret");
+app.get("/secret", loginRequired, (req, res) => {
+	if (!(req.session && req.session.userId)) {
+		res.redirect("/login");
+	}
+
+	User.findById(req.session.userId, (err) => {
+		if (err) {
+			console.log(err);
+			res.redirect("/login");
+		}
+		res.render("secret");
+	});
+
 });
 
 
 //Auth Routes
 
 //show sign up form
-app.get("/register", (req, res) => {
-	res.render("register");
+app.get("/register", csrfProtection, (req, res) => {
+	res.render("register", { csrfToken: req.csrfToken() });
 });
 
 //handle user signup
-app.post("/register", (req, res) => {
-	User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+app.post("/register", csrfProtection, (req, res) => {
+	let hash = bcrypt.hashSync(req.body.password, 14);
+	req.body.password = hash;
+	let user = new User(req.body);
+	user.save((err) => {
 		if(err) {
 			console.error(err);
 			res.render('register');
 		} else {
-			passport.authenticate("local")(req, res, () => {
-				res.redirect("/secret");
-			});
+			res.redirect("/");
 		}
 	});
 });
 
 //LOGIN ROUTES
 //render login form
-app.get("/login", (req, res) => {
-	res.render('login');
+app.get("/login", csrfProtection, (req, res) => {
+	res.render('login', { csrfToken: req.csrfToken() });
 });
 
 //login logic
-app.post("/login", passport.authenticate("local", {
-	failureRedirect: "/login"
-}), (req, res) => {
-	res.redirect("/secret");
+app.post("/login", csrfProtection, (req, res) => {
+	User.findOne({username: req.body.username}, (err, user) => {
+		if (err || !user || !bcrypt.compareSync(req.body.password, user.password)) {
+			return res.render("login", {
+				error: "Incorrect email/password."
+			});
+		}
+		
+		req.session.userId = user._id;
+		res.redirect("/secret");
+	});
 });
 
 //Log Out
 app.get("/logout", (req, res) => {
-	req.logout();
+	req.session.userId = null;
 	res.redirect("/");
 });
 
-
-
-function isLoggedIn(req, res, next) {
-	console.log("isLoggedIn called");
-	if(req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/login");
-}
 
 
 
