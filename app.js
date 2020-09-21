22dfc31cff7f91137082a4719600f25b11d79746
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-09-20 20:46:44 -0400

    Complete through part 5

--- app.js
+++ app.js
@@ -17,17 +17,18 @@ mongoose.connect("mongodb://localhost:27017/auth_demo");
 const app = express();
 app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(expressSession({
-	secret: "Rusty is the best and cutest dog in the world",
+	secret: "This is so random...",
 	resave: false,
 	saveUninitialized: false
 }));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 //======
 //ROUTES
@@ -37,7 +38,7 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", isLoggedIn, (req, res) => {
 	res.render("secret");
 });
 
@@ -63,6 +64,34 @@ app.post("/register", (req, res) => {
 	});
 });
 
+//LOGIN ROUTES
+//render login form
+app.get("/login", (req, res) => {
+	res.render('login');
+});
+
+//login logic
+app.post("/login", passport.authenticate("local", {
+	failureRedirect: "/login"
+}), (req, res) => {
+	res.redirect("/secret");
+});
+
+//Log Out
+app.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect("/");
+});
+
+
+
+function isLoggedIn(req, res, next) {
+	console.log("isLoggedIn called");
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+}
 
 
 
